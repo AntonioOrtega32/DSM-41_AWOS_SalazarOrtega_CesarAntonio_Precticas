c69e8b854d018695e6a2b7185d9b340352b06807
AntonioOrtega <al222110788@gmail.com>
2022-09-30 02:46:15 -0500

    Practica

---
+++ Practicas/database/factories/PostFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Nette\Utils\Random;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PostFactory extends Factory
+{
+    /**
+     *@var string
+     */
+    protected $model = Post::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            'tilte' => $this-> faker -> sentence,
+            'slug' => $this->faker -> slug,
+            'descripcion' => $this ->faker -> text(80),
+            'image' => $this-> faker -> imageUrl(1280,720),
+            'metatag' => $this-> faker -> text(7),
+            'autor' => $this -> faker -> sentence,
+            'date' => $this -> faker -> dateTime(),
+            //Llaves foraneas 
+            'user_id' => random_int(1,100),
+            'category_id' => random_int(1,100),
+        ];
+    }
+}
---
+++ Practicas/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory(10)->create();
+        Category::factory(10)->create();
+        Post::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
+    }
+}
